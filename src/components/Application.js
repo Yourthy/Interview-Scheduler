3300485e3730744c7fbdf85a5d7669cd9590d9f4
christopher <christopher.t.yourth@gmail.com>
2021-01-19 23:18:42 -0700

    first commit (very overdue). Added DayListItem/DayList, InterviewerListItem/InterviewerList and have recently started on appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
+import DayList from "./DayList.js";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
+  
   return (
     <main className="layout">
       <section className="sidebar">
+      <img
+        className="sidebar--centered"
+        src="images/logo.png"
+        alt="Interview Scheduler"
+      />
+      <hr className="sidebar__separator sidebar--centered" />
+      <nav className="sidebar__menu">
+      <DayList days={days} setDay={setDay} day={day}/>
+      </nav>
+      <img
+        className="sidebar__lhl sidebar--centered"
+        src="images/lhl.png"
+        alt="Lighthouse Labs"
+      />
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
